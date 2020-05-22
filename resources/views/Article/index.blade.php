95cccd6b007679e6c9874f4b97e0b6f1daf84020
imadfouad <imadfouad48@gmail.com>
2020-05-22 05:25:54 +0000

    ajout select

--- resources/views/Article/index.blade.php
+++ resources/views/Article/index.blade.php
@@ -4,6 +4,7 @@
     <title> Articles </title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/test.index.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -240,8 +241,9 @@
                         <input type="hidden" name="id_article" value="{{ $article->id_article }}" >
                         <input type="hidden" name="prix_unit"  value="{{ $article->prix }}" >
                         
+    <a style="color: white;" class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" > <i class="fas fa-cart-plus"></i> </a>
 
-   <input type="button"   class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" value="Ajouter au Panier" > 
+   
     
                         
                         
@@ -344,7 +346,7 @@
                         <input type="hidden" name="prix_unit" value="{{ $article->prix }}" >
                         
 
-    <input type="button"   class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" value="Ajouter au Panier" >     
+    <a style="color: white;" class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" > <i class="fas fa-cart-plus"></i> </a>     
                         
                         
                     </form>
@@ -417,7 +419,7 @@
 
 
 
-    <input type="button"   class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" value="Ajouter au Panier" >
+    <a style="color: white;" class=" btn btn-success transp"  onclick="mafonction({{$article->id_article}});" > <i class="fas fa-cart-plus"></i> </a>
                     </form>
                         
                         
@@ -533,7 +535,7 @@
     
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script src="{{ asset('js/app.js') }}"></script>
-
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/js/all.min.js"> </script>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
